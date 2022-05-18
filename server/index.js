b17075ed3f16dbb9df0d0fa5daeea60ed0495df9
Your Name <you@example.com>
2022-05-18 15:03:29 -0600

    second commit

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+app.get("/styles", (req, res) => {
+    res.SendFile(path.join(__dirname, '../public/index.css'));
+})
+
 
 const port = process.env.PORT || 4005;
 
