9c9bfca7f745fe530b682a6e0812d96cd8311292
Your Name <you@example.com>
2022-05-19 15:21:33 -0600

    idk

--- server/index.js
+++ server/index.js
@@ -7,6 +7,15 @@ require('dotenv').config()
 
 const app = express();
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '455c442c1e004a7da665cb8219ac7aeb',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+rollbar.log('Hello world!')
+
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.get("/", (req, res) => {
